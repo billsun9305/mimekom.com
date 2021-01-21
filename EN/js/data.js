bd06366719f7f4ad423abd2c0ca86a64857014de
Bill <billsun9305@gmail.com>
2021-01-21 17:56:05 +0800

    blog data.js

--- EN/js/data.js
+++ EN/js/data.js
@@ -3,7 +3,7 @@ var data = [
 		"Id": "1",
 		"Title": "Super secure hardware encrypted communication APP is officially launched - MIMEKOM",
 		"Date": "Date 2020-10-20",
-		"Thumbnail": true,
+		"Thumbnail": false,
 		"Tags": "super secure encrypted communication / mimekom / MIMEKOM / Chip manager / for information security / NIST /FIPS 140-2 Level 3/ safer communication enviornment / officially launched / Slim SIM / slim sim",
 		"Content": "<img><br>The MIMEKOM website is officially launched, and the super secure encrypted communication APP will be launched soon. Focus on the information security, we hope to provide a new experience and safer service on the communication app.<br><br>In the generation of advanced communication software, information security issues become a concern for companies or individuals to communicate on communication APPs. MIMEKOM is a secure communication APP born for information security and with a hardware security chip which is certified by NIST for FIPS 140-2 level 3. The hardware encryption Slim SIM is able to offer the safer communication enviornment.<br><br>MIMEKOM brings end-user the secure communication anytime/anywhere, and invites you to join the world of information security!"
 	}
